4edc793aaf33c8f4170933f8e46a0a61f47d1201
Slot <1059819898@qq.com>
2021-02-13 19:19:05 +0800

    debug some errors about null data

--- lib/screen/findmusic/components/play_list_desc_dialog.dart
+++ lib/screen/findmusic/components/play_list_desc_dialog.dart
@@ -1,12 +1,11 @@
 import 'dart:ui';
-import 'package:extended_image/extended_image.dart';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:neatease_app/entity/sheet_details_entity.dart';
 import 'package:neatease_app/util/cache_image.dart';
 import 'package:neatease_app/widget/common_text_style.dart';
 import 'package:neatease_app/widget/v_empty_view.dart';
-import 'package:neatease_app/widget/widget_tag.dart';
 
 class PlayListDescDialog extends StatelessWidget {
   final SheetDetailsPlaylist _data;
@@ -71,21 +70,21 @@ class PlayListDescDialog extends StatelessWidget {
                             width: ScreenUtil().screenWidth * 3 / 4,
                           ),
                           VEmptyView(20),
-                          _data.tags.isEmpty
-                              ? Container()
-                              : Row(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    Text(
-                                      '标签：',
-                                      style: common14WhiteTextStyle,
-                                    ),
-                                    ..._data.tags
-                                        .map((t) => TagWidget(t))
-                                        .toList()
-                                  ],
-                                ),
-                          _data.tags.isEmpty ? Container() : VEmptyView(20),
+                          // _data.tags.isEmpty
+                          //     ? Container()
+                          //     : Row(
+                          //         crossAxisAlignment: CrossAxisAlignment.start,
+                          //         children: <Widget>[
+                          //           Text(
+                          //             '标签：',
+                          //             style: common14WhiteTextStyle,
+                          //           ),
+                          //           ..._data.tags
+                          //               .map((t) => TagWidget(t))
+                          //               .toList()
+                          //         ],
+                          //       ),
+                          // _data.tags.isEmpty ? Container() : VEmptyView(20),
                           Text(
                             _data.description,
                             style: common14WhiteTextStyle,
