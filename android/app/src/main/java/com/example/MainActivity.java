2e4d2aacc5de297527d6091723c04fa14be530ec
Slot <1059819898@qq.com>
2021-02-22 23:54:12 +0800

    update 返回桌面不退出程序

---
+++ android/app/src/main/java/com/example/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.neatease_app;
+
+import androidx.annotation.NonNull;
+
+
+import io.flutter.embedding.android.FlutterActivity;
+import io.flutter.embedding.engine.FlutterEngine;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+    public static MainActivity activity;
+
+    @Override
+    public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
+        super.configureFlutterEngine(flutterEngine);
+        activity = this;
+        String CHANNEL = "android/back/desktop";
+        new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL).setMethodCallHandler(
+                (methodCall, result) -> {
+                    if (methodCall.method.equals("backDesktop")) {
+                        result.success(true);
+                        moveTaskToBack(false);
+                    }
+                }
+        );
+    }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        GeneratedPluginRegistrant.registerWith(this);
+//        activity = this;
+//        FlutterStarrySkyPlugin.registerWith(this.registrarFor("flutter_starry_sky"));
+//        //通讯名称,回到手机桌面
+//
+//    }
+
+
+
+
+
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
+
